48e806366200a38195e8bc891b2c1a7024cce339
Ciel <daniel36vuong@gmail.com>
2020-05-14 22:33:54 -0700

    Have a scene transition from menu to level; replaced PSD files with PNG files.

--- NorcoCollege/Assets/Daniel/Scripts/Scenes.cs
+++ NorcoCollege/Assets/Daniel/Scripts/Scenes.cs
@@ -2,29 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Scenes : MonoBehaviour
 {
     public GameObject startButton;
-    public static int sceneCountInBuildSettings;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(sceneCountInBuildSettings);
+        Button btn = startButton.GetComponent<Button>();
+        btn.onClick.AddListener(StartGame);
     }
 
     // Update is called once per frame
     void Update()
     {
-        OnMouseDown(startButton);
+
     }
 
-    public void OnMouseDown(GameObject st)
+    public void StartGame()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            SceneManager.LoadScene("New Prototype Level");
-        }
+        SceneManager.LoadScene(1);
     }
 }
